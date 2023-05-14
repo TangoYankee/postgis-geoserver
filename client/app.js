a4251ec64cb3f1cf78a1e21456d491345f7f959d
tangoyankee <miller.tim108@gmail.com>
2023-05-13 20:01:11 -0400

    place header on page

--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
+const DEFAULT_LAT = 40.7128;
+const DEFAULT_LNG = -74.0060;
+const DEFAULT_ZOOM = 11; 
+
 const app = () => {
-    console.info('content loaded')
-    const map = L.map('map').setView([40.74, -74.0], 13);
+    const map = L.map('map').setView([DEFAULT_LAT, DEFAULT_LNG], DEFAULT_ZOOM);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
