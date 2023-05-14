0ab7d2ae4408d577ae1d102ba684014a31f0455c
tangoyankee <miller.tim108@gmail.com>
2023-05-13 22:27:21 -0400

    request green roof and communes data

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 const HOST = 'http://localhost:5050';
 const LAB_PATH = 'geoserver/lab_14';
 const LAB_URL = `${HOST}/${LAB_PATH}`;
+
 const DEFAULT_LAT = 40.7128;
 const DEFAULT_LNG = -74.0060;
 const DEFAULT_ZOOM = 11; 
@@ -40,6 +41,50 @@ const app = () => {
             transparent: true,
         }
     ).addTo(map);
+
+    const greenRoofParameters = L.Util.extend({
+        service: 'WFS',
+        version: '2.0',
+        request: 'GetFeature',
+        typeName: 'lab_14:GreenRoofData2016_20180917',
+        outputFormat: 'application/json',
+        format_options: 'callback:getJson',
+        SrsName: 'EPSG:2261',
+        transparent: 'true'
+    });
+
+    const owsUrl = `${LAB_URL}/ows`;
+    const greenRoofUrl = `${owsUrl}${L.Util.getParamString(greenRoofParameters)}`;
+
+    const greenRoofRequest = new XMLHttpRequest();
+    greenRoofRequest.open('GET', greenRoofUrl);
+    greenRoofRequest.setRequestHeader ('Content-type', 'application / x-www-form-urlencoded');
+    greenRoofRequest.addEventListener('load', () => {
+        const result = JSON.parse(greenRoofRequest.response);
+        L.Proj.geoJson(result, crs).addTo(map);
+    });
+    greenRoofRequest.send();
+
+    const communesParameters = L.Util.extend({
+        service: 'WFS',
+        version: '2.0',
+        request: 'GetFeature',
+        typeName: 'sig_libre:communes_idf',
+        outputFormat: 'application/json',
+        format_options: 'callback:getJson',
+        SrsName: 'EPSG:2261',
+        transparent: 'true'
+    });
+
+    const communesUrl = `${owsUrl}${L.Util.getParamString(communesParameters)}`;
+    const communesRequest = new XMLHttpRequest();
+    communesRequest.open('GET', communesUrl);
+    communesRequest.setRequestHeader ('Content-type', 'application / x-www-form-urlencoded');
+    communesRequest.addEventListener('load', () => {
+        const result = JSON.parse(communesRequest.response);
+        L.Proj.geoJson(result, crs).addTo(map);
+    });
+    communesRequest.send();
 }
 
 window.addEventListener('DOMContentLoaded', app);
