54508f163589820fca9dbcc652b6005f8b9f5e8d
tangoyankee <miller.tim108@gmail.com>
2023-05-13 20:05:21 -0400

    place project land cover tiff on map

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+const HOST = 'http://localhost:5050';
 const DEFAULT_LAT = 40.7128;
 const DEFAULT_LNG = -74.0060;
 const DEFAULT_ZOOM = 11; 
@@ -14,6 +15,29 @@ const app = () => {
         fillColor: '# f03',
         fillOpacity: 0.5,
     }).addTo(map)
+
+    const crs = new L.Proj.CRS('EPSG: 2261',
+        '+proj=tmerc +lat_0=40 +lon_0=-76.58333333333333 +k=0.9999375 \
+        +x_0=249999.9998983998 +y_0=0 +ellps=GRS80 +datum=NAD83 \
+        +to_meter=0.3048006096012192 +no_defs',
+        {
+            resolutions:[
+                8192, 4096, 2048, 1024, 512, 256, 128
+            ],
+            origin: [0, 820208],
+            bounds: L.bounds([503103.0219, 730813.7225], [1231541.2872, 1607028.5833])
+        }
+    );
+
+    L.tileLayer.wms (
+        `${HOST}/geoserver/lab_14/wms`,
+        {
+            crs: crs,
+            layers: 'lab_14:NYClandcover2010',
+            format: 'image/png',
+            transparent: true,
+        }
+    ).addTo(map);
 }
 
 window.addEventListener('DOMContentLoaded', app);
