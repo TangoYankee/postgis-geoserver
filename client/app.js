90dbc9aff9fc37b1e7adbecc0f0373fced1123fc
tangoyankee <miller.tim108@gmail.com>
2023-05-13 20:03:47 -0400

    place markers on map

--- client/app.js
+++ client/app.js
@@ -8,6 +8,12 @@ const app = () => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    L.marker ([40.7128, -74.0060]).addTo(map);
+    L.circle ([40.7128, -74.0060], 2500, {
+        color: 'red',
+        fillColor: '# f03',
+        fillOpacity: 0.5,
+    }).addTo(map)
 }
 
 window.addEventListener('DOMContentLoaded', app);
